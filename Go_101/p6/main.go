c915597f58f0053bb10ce8b3eeda003df0ea62ca
PrikPrak <PrikPrak@users.noreply.github.com>
2014-04-13 23:25:05 -0400

    Completely finished Problem 6.

--- Go_101/p6/main.go
+++ Go_101/p6/main.go
@@ -13,19 +13,30 @@ func *function name*(*argument name and data type*) (*return parameters, name th
 package main
 
 //importing fmt for file I/O and os for opening file. 
-import(
-fmt
-os
-)
+import "fmt"
+import "os"
 
-
-func main(){
-
+func main(){        
   // Open, used for reading,
-  //takes a string, returns a pointer to a file (file) & error (err)
-  filePtr := os.Open("testdata6")
+  // Takes a string, returns a pointer to a file (file) & error (err)
+  filePtr, fErr := os.Open("testdata6")
+  	// If fErr is not nil, then it'll be a *PathError.
+    if fErr != nil {
+			fmt.Println(fErr)
+    }
 
-  
+  // Dynamically create a slice of 100 bytes.
+	data := make([]byte, 100)
 
-  
+  // Read the file. Number of bytes depends on the size of the slice. 
+  // count will receive Read's return value of the number of bytes read. 
+	count, cErr := filePtr.Read(data)
+  // If xErr is not nil, then it'll be a *PathError. 
+  if cErr != nil {
+		fmt.Println(fErr)
+  }
+
+  // Print the number of bytes read, as well as the data.
+  // I can't find any documentation on wth data[] is. 
+  fmt.Printf("read %d bytes: %q\n", count, data[:count])
 }
