ce9797071eda03d7091f9842b2f5f05e69f0a6a5
PrikPrak <PrikPrak@users.noreply.github.com>
2014-05-05 00:45:26 -0400

    examples/gowiki: added editing and saving functionality.

--- examples/gowiki/wiki.go
+++ examples/gowiki/wiki.go
@@ -45,12 +45,16 @@ func loadPage(title string) (*Page, error) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
   title := r.URL.Path[len("/view/"):]
-  p, _ := loadPage(title)
+  p, err := loadPage(title)
+  if err != nil {
+    http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+    return
+  }
 
   renderTemplate(w, "view", p)
 }
 
-func edithandler(w http.ResponseWriter, r *http.Request) {
+func editHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
   p, err := loadPage(title)
   if err != nil {
@@ -65,9 +69,18 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
   t.Execute(w, p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/save/"):]
+
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  p.save()
+  http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
   http.HandleFunc("/view/", viewHandler)
-//  http.HandleFunc("/edit/", editHandler)
-//  http.HandleFunc("/save/", saveHandler)
+  http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHandler)
   http.ListenAndServe(":8080", nil)
 }
