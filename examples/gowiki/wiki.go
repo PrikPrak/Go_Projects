f8d6b9463a4fa007badb99b517b595402a6140ff
PrikPrak <PrikPrak@users.noreply.github.com>
2014-05-06 00:16:55 -0400

    examples/gowiki: Added validation to path.
    Created a regexp.Regexp variable as a global variable and
    created a function to utilize it, called getTitle(). Then,
    the handler functions were refactored to use this function
    to retrieve the titles for their respective pages.

--- examples/gowiki/wiki.go
+++ examples/gowiki/wiki.go
@@ -15,9 +15,13 @@ import (
   "html/template"
   "io/ioutil"
   "net/http"
+  "regexp"
+  "errors"
 )
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 //Body is a byte slice because it's the type expected by io libraries.
 type Page struct {
@@ -47,7 +51,11 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/view/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
+
   p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -58,8 +66,12 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   p, err := loadPage(title)
+
   if err != nil {
     p = &Page{Title: title}
   }
@@ -78,18 +90,31 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/save/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
 
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
 
-  err := p.save()
+  err = p.save()
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
   }  
 
   http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+  m := validPath.FindStringSubmatch(r.URL.Path)
+  if m == nil {
+    http.NotFound(w, r)
+    return "", errors.New("Invalid Page Title")
+  }
+
+  return m[2], nil //The title is the second subexpression. 
 }
 
 func main() {
